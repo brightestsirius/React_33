c03d5efe451867a12c31dba88e9bc74f710dde56
brightestsirius <katherina.shipovskaya@boosta.com>
2026-08-31 16:47:46 +0300

    lesson_15 - bootstrap

--- lesson_15_bootstrap/gulpfile.js
+++ lesson_15_bootstrap/gulpfile.js
@@ -8,30 +8,47 @@ const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 
 // DEV
-function styles() {
-  return src("./sass/main.sass")
-    .pipe(sourcemaps.init())
-    .pipe(sass().on("error", sass.logError))
-    .pipe(postcss([autoprefixer()]))
-    .pipe(rename("style.css"))
-    .pipe(sourcemaps.write("."))
-    .pipe(dest("./styles"));
+function makeStylesDev(srcPath, outName) {
+  const task = () =>
+    src(srcPath)
+      .pipe(sourcemaps.init())
+      .pipe(sass().on("error", sass.logError))
+      .pipe(postcss([autoprefixer()]))
+      .pipe(rename(outName))
+      .pipe(sourcemaps.write("."))
+      .pipe(dest("./styles"));
+  task.displayName = `styles:${outName}`;
+  return task;
 }
 
 // PRODUCTION
-function stylesProd() {
-  return src("./sass/main.sass")
-    .pipe(sass().on("error", sass.logError))
-    .pipe(postcss([autoprefixer()]))
-    .pipe(cleanCSS())
-    .pipe(rename("style.css"))
-    .pipe(dest("./styles"));
+function makeStylesProd(srcPath, outName) {
+  const task = () =>
+    src(srcPath)
+      .pipe(sass().on("error", sass.logError))
+      .pipe(postcss([autoprefixer()]))
+      .pipe(cleanCSS())
+      .pipe(rename(outName))
+      .pipe(dest("./styles"));
+  task.displayName = `styles:${outName}:prod`;
+  return task;
 }
+
+const styles = makeStylesDev("./sass/main.sass", "style.css");
+const stylesCustomColorsSass = makeStylesDev(
+  "./sass/custom-colors-sass.sass",
+  "custom-colors-sass.css"
+);
+const stylesProd = makeStylesProd("./sass/main.sass", "style.css");
+const stylesCustomColorsSassProd = makeStylesProd(
+  "./sass/custom-colors-sass.sass",
+  "custom-colors-sass.css"
+);
 
 // WATCH
 function watchFiles() {
-  watch("./sass/**/*.sass", styles);
+  watch("./sass/**/*.sass", series(styles, stylesCustomColorsSass));
 }
 
-exports.dev = series(styles, watchFiles);
-exports.build = series(stylesProd);
+exports.dev = series(styles, stylesCustomColorsSass, watchFiles);
+exports.build = series(stylesProd, stylesCustomColorsSassProd);
