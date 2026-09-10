12897f15fef8620447b188c97ad92f60110d815a
brightestsirius <katherina.shipovskaya@boosta.com>
2026-09-10 21:25:33 +0300

    lesson_18

---
+++ lesson_18/script.js
@@ -0,0 +1,62 @@
+// var message = "Hello",
+//   age = 32,
+//   year = "2026";
+
+// console.log(message, age, year);
+// console.log(message + ", I'm " + age + " years old. I was born in " + year); // Hello, I'm 32 years old. I was born in 2026
+
+// let message = "Hello";
+// message = 100;
+// console.log(message);
+
+// let userName = "Katya";
+
+// string
+let userName = "Oleg";
+
+//number
+let userAge = 32;
+let percentage = 0.567;
+let minus = -42;
+
+// boolean
+let isMerried = true;
+let hasChild = false;
+
+let presidentName; // undefined
+
+// Object
+let student = {
+  name: "Oleg",
+  age: 32,
+  isMarried: true,
+  city: "Kyiv",
+  child: {
+    name: "Anna",
+    age: 5,
+  },
+};
+
+// console.log(student);
+
+let students = ["Oleg", "Katya", "Anna", "Oleg", "Anna"];
+
+let ages = [32, 28, 5];
+
+let animals = [
+  {
+    name: "Lion",
+    age: 5,
+  },
+  {
+    name: "Elephant",
+    age: 10,
+  },
+  {
+    name: "Giraffe",
+    age: 7,
+  },
+];
+
+let sum = 10 + 5;
+console.log(sum); //15
