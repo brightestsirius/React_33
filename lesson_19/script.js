57d670c459128db98f7b427e1bc5fb20ebe694cd
brightestsirius <katherina.shipovskaya@boosta.com>
2026-09-14 21:28:35 +0300

    lesson_19

---
+++ lesson_19/script.js
@@ -0,0 +1,84 @@
+// let firstName = `Taras`,
+//   secondName = `Sheva`,
+//   fullName = `${firstName} ${secondName}`,
+//   yearOfBirth = 1994;
+
+// console.log(fullName);
+// console.log(
+//   ` Hello, world!
+//     My name is ${fullName}.
+//     I'm ${2026 - yearOfBirth} years old.`,
+// );
+
+// let value = `Hello, world!`;
+
+// // Hello
+// // 01234 => 5-1
+
+// let firstLetter = value[0]; // H
+// let secondLetter = value[1]; // e
+// let someLeteter = value[100]; // undefined
+
+// console.log(firstLetter);
+// console.log(secondLetter);
+// console.log(someLeteter);
+
+// console.log(value.length);
+
+// let lastIndex = value.length-1; // 12
+// let lastLetter = value[lastIndex];
+// console.log(lastLetter);
+
+// let text = 'Привіт, світе!';
+// let partOfText = text.substring(0, 6);
+
+// console.log(partOfText);
+// console.log(text);
+
+// let text = 'Привіт, світе!';
+// console.log(text.slice(0, 6));
+// console.log(text);
+
+// var text = "Привіт, світе!";
+// console.log(text.substr(0, 5));
+
+// var text = "Привітс, світ світе!";
+// console.log(text.indexOf("світе"));
+
+// let text = "Привіт, світе!";
+// let textReplaced = text.replace("світе", "друже");
+
+// console.log(text);
+// console.log(textReplaced);
+
+// let value = `Hello, world, cat, dog`;
+
+// let modifiedValue = value.split(`, `);
+
+// console.log(value);
+// console.log(modifiedValue);
+
+// let value = `Hello, world, cat, dog`,
+//   upperValue = value.toUpperCase(),
+//   lowerValue = value.toLowerCase();
+
+// console.log(value);
+// console.log(upperValue);
+// console.log(lowerValue);
+
+// let value = `hello World CAT dog`;
+
+// let firstLetter = value.charAt(0).toUpperCase(); // H
+// let allLeters = value.slice(1).toLowerCase(); // `ello World CAT dog`
+// let modifiedValue = `${firstLetter}${allLeters}`; // firstLetter + allLeters;
+
+// console.log(modifiedValue);
+
+// console.log(10 + +"5"); // 10 + 5 => 15
+// console.log(10 - "5");
+// console.log(10 * "5");
+// console.log(10 / "5");
+
+let a = "10",
+  b = 5,
+  sum = +a + +b;
